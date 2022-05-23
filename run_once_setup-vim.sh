29363e6fff10853cb57e3b82b9162c3157738c40
Clay McGinnis <github@clay.sh>
2022-05-22 17:04:12 -0700

    lua nvim

--- run_once_setup-vim.sh
+++ run_once_setup-vim.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# Create directories for use with vim
-mkdir -p ~/.vim/tmp/undo
-mkdir -p ~/.vim/tmp/backup
-mkdir -p ~/.vim/tmp/swp
 
 # Create cron job to purge undofiles if source file is deleted
 crontab=crontab -l 2>/dev/null # Gets cron jobs
