f47e4e03efc64dd8ac6f6681c2eb69fe51763ed9
Clay McGinnis <github@clay.sh>
2025-02-05 16:36:51 -0800

    chore: switch zoom and slack to brew since they change dir on update

--- hosts/work-macbook/default.nix
+++ hosts/work-macbook/default.nix
@@ -1,9 +1,14 @@
-{ pkgs, ... }@inputs: {
-  users.users.clay = { home = "/Users/clay"; };
-  homebrew.casks = [ "monokle" ];
+{ pkgs, ... }@inputs:
+{
+  users.users.clay = {
+    home = "/Users/clay";
+  };
+  homebrew.casks = [
+    "monokle"
+    "zoom"
+    "slack"
+  ];
   environment.systemPackages = with pkgs.unstable; [
-    zoom-us
-    slack
     awscli2
     kubernetes-helm
     pre-commit
@@ -12,8 +17,8 @@
     "/System/Applications/Mail.app"
     "/Applications/Notion Calendar.app"
     "/Applications/Notion.app"
-    "${zoom-us}/Applications/zoom.us.app"
-    "${slack}/Applications/Slack.app"
+    "/Applications/zoom.us.app"
+    "/Applications/Slack.app"
     "/Applications/Zen Browser.app"
     "/Applications/Cursor.app"
   ];
