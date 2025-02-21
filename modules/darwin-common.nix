4e8978a708fb83c296eac1908332a5819133fd52
Clay McGinnis <github@clay.sh>
2025-02-21 11:48:35 -0800

    feat: new software, setting

--- modules/darwin-common.nix
+++ modules/darwin-common.nix
@@ -1,4 +1,11 @@
-{ config, nixpkgs, pkgs, self, ... }@inputs: {
+{
+  config,
+  nixpkgs,
+  pkgs,
+  self,
+  ...
+}@inputs:
+{
   nix-homebrew = {
     enable = true;
     enableRosetta = true;
@@ -17,31 +24,35 @@
   # system.configurationRevision = self.rev or self.dirtyRev or null;
 
   # Activation script to create aliases for applications
-  system.activationScripts.applications.text = let
-    env = pkgs.buildEnv {
-      name = "system-applications";
-      paths = config.environment.systemPackages;
-      pathsToLink = "/Applications";
-    };
-  in pkgs.lib.mkForce ''
-    # Set up applications.
-    echo "setting up /Applications..." >&2
-    rm -rf /Applications/Nix\ Apps
-    mkdir -p /Applications/Nix\ Apps
-    find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-    while read -r src; do
-      app_name=$(basename "$src")
-      echo "copying $src" >&2
-      ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-    done
-  '';
+  system.activationScripts.applications.text =
+    let
+      env = pkgs.buildEnv {
+        name = "system-applications";
+        paths = config.environment.systemPackages;
+        pathsToLink = "/Applications";
+      };
+    in
+    pkgs.lib.mkForce ''
+      # Set up applications.
+      echo "setting up /Applications..." >&2
+      rm -rf /Applications/Nix\ Apps
+      mkdir -p /Applications/Nix\ Apps
+      find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+      while read -r src; do
+        app_name=$(basename "$src")
+        echo "copying $src" >&2
+        ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+      done
+    '';
 
   # Create /etc/zshrc that loads the nix-darwin environment.
   programs.zsh.enable = true; # default shell on catalina
   # programs.fish.enable = true;
   security.pam.enableSudoTouchIdAuth = true;
 
-  users.users.clay = { home = "/Users/clay"; };
+  users.users.clay = {
+    home = "/Users/clay";
+  };
 
   system.defaults = {
     CustomSystemPreferences.NSGlobalDomain."com.apple.mouse.linear" = true;
@@ -88,7 +99,10 @@
       "private-internet-access"
       "tailscale"
       "vivaldi"
+      "yaak"
     ];
-    masApps = { Bitwarden = 1352778147; };
+    masApps = {
+      Bitwarden = 1352778147;
+    };
   };
 }
